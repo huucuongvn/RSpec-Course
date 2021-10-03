c1dacf0ce31ef3d2f01e429f1cfbfe5071662a88
huucuongvn <huucuongbeat95@gmail.com>
2021-10-03 21:27:27 +0700

    Reducing duplication - Before hooks and instance variables

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,13 +1,21 @@
 class Card
-    attr_accessor :type
-    def initialize(type)
-        @type = type
+    attr_accessor :rank, :suit
+    def initialize(rank, suit)
+        @rank = rank
+        @suit = suit
     end
 end
 
-RSpec.describe 'Card' do
-    it 'has a type' do
-        card = Card.new('Ace of spades')
-        expect(card.type).to eq('Ace of spades')
+RSpec.describe Card do
+    before do
+        @card = Card.new('Ace', 'Spades')
+    end
+
+    it 'has a rank' do
+        expect(@card.rank).to eq('Ace')
+    end
+
+    it 'has a suit' do
+        expect(@card.suit).to eq('Spades')
     end
 end
