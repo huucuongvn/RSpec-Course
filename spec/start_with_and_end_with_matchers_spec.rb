99e44f2375a7a1135a5eb2dbafbb730d4ca203aa
huucuongvn <huucuongbeat95@gmail.com>
2021-10-07 22:18:05 +0700

    start_with and end_with matchers

---
+++ spec/start_with_and_end_with_matchers_spec.rb
@@ -0,0 +1,18 @@
+RSpec.describe 'start_with and end_with matchers' do
+    describe 'caterpillar' do
+        it 'should check for substring at the beginning or end' do
+            expect(subject).to start_with('cat')
+            expect(subject).to end_with('llar')
+        end
+        
+        it {is_expected.to start_with('cate')}
+        it {is_expected.to end_with('lar')}
+    end
+
+    describe [:a, :b, :c, :d] do
+        it 'should check for elements at beginning or end of the array' do
+            expect(subject).to start_with(:a)
+        end
+    end
+
+end
